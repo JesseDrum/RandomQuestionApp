3637d81fa5896fbc73bb1892486c486a48e9cd1e
Jesse <Jessedrum07@gmail.com>
2022-02-07 10:24:13 -0500

    Initial Commit

---
+++ RandomQuestionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomQuestionApp
+//
+//  Created by Drum, Jesse on 2/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
